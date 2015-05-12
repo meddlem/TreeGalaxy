fd28f3433ff90b35a2c9904e9fde731781afdc55
meddlem <mprebergen@gmail.com>
2015-05-12 07:58:57 +0200

    tree shows promise

--- tree.f95
+++ tree.f95
@@ -24,9 +24,9 @@ contains
     type(node), pointer, intent(inout) :: N
     real(dp), intent(in)               :: p(2)
 
-    type(node), pointer :: child_N
-    real(dp)            :: neworigin(2), p_old(2)
-    integer             :: i
+    type(node), pointer :: child_1, child_2, child_3, child_4, Next_N
+    real(dp)   :: neworigin(2), p_old(2)
+    integer    :: i
 
     if (N%leaf_node) then
       ! if we are in a leaf node, ie no child nodes
@@ -38,6 +38,15 @@ contains
         ! save position data
         p_old = N%loc
         N%leaf_node = .false.
+        ! allocate children
+        allocate(child_1)
+        allocate(child_2)
+        allocate(child_3)
+        allocate(child_4)
+        N%quadrant1 => child_1
+        N%quadrant2 => child_2
+        N%quadrant3 => child_3
+        N%quadrant4 => child_4
 
         do i = 1,4
           neworigin(1) = N%origin(1) + &
@@ -45,64 +54,64 @@ contains
           neworigin(2) = N%origin(2) + &
             N%halfDim/sqrt(2._dp) * merge(1, -1, i < 3)
           
-          allocate(child_N)
-
-          child_N%origin = neworigin
-          child_N%halfDim = N%halfDim/2._dp
-
           if (i == 1) then
-            N%quadrant1 => child_N
+            child_1%origin = neworigin
+            child_1%halfDim = N%halfDim/2._dp
+
           elseif (i == 2) then 
-            N%quadrant2 => child_N
+            child_2%origin = neworigin
+            child_2%halfDim = N%halfDim/2._dp
           elseif (i == 3) then 
-            N%quadrant3 => child_N
+            child_3%origin = neworigin
+            child_3%halfDim = N%halfDim/2._dp
           else 
-            N%quadrant4 => child_N
+            child_4%origin = neworigin
+            child_4%halfDim = N%halfDim/2._dp
           endif
         enddo
         
         ! now we need to quadrant the points are in (p_temp and p) and
         ! insert them in the child nodes that we just created
-        call get_quadrant(p_old, N, Child_N)
-        call insert_particle(p_old, Child_N)
+        call get_quadrant(p_old, N, Next_N)
+        call insert_particle(p_old, Next_N)
 
-        call get_quadrant(p, N, Child_N)
-        call insert_particle(p, Child_N)
+        call get_quadrant(p, N, Next_N)
+        call insert_particle(p, Next_N)
       endif
     else
       ! we are at an interior node,
       ! insert into one of its quadrants
-      call get_quadrant(p, N, Child_N)
-      call insert_particle(p, Child_N) 
+      call get_quadrant(p, N, Next_N)
+      call insert_particle(p, Next_N) 
     endif
   end subroutine
 
   subroutine get_quadrant(p, N, Child_N)
-    type(node), pointer, intent(inout) :: Child_N
-    type(node), pointer, intent(in)    :: N
-    real(dp), intent(in)               :: p(2)
+    ! returns pointer to quadrant where p is located
+    type(node), pointer  :: Child_N
+    type(node), pointer  :: N
+    real(dp), intent(in) :: p(2)
 
     if(p(1) > N%origin(1) .and. p(2) < N%origin(2)) then
-      Child_N = N%quadrant2
+      Child_N => N%quadrant2
     elseif(p(1) < N%origin(1) .and. p(2) > N%origin(2)) then
-      Child_N = N%quadrant4
+      Child_N => N%quadrant4
     elseif(p(1) < N%origin(1) .and. p(2) < N%origin(2)) then
-      Child_N = N%quadrant3
+      Child_N => N%quadrant3
     else 
-      Child_N = N%quadrant1
+      Child_N => N%quadrant1
     endif
   end subroutine
 
   ! lists locations of particles contained in the tree
   recursive subroutine getpoints(N)
-    type(node), pointer, intent(in) :: N
+    type(node), pointer :: N
 
-    type(node), pointer :: Child_N
+    !type(node), pointer :: Child_N
 
     if (N%leaf_node .and. N%contains_particle) then
       print *, N%loc
-    elseif(N%contains_particle) then
-      allocate(child_N)
+    elseif(.not. N%leaf_node) then
       call getpoints(N%quadrant1)
       call getpoints(N%quadrant2)
       call getpoints(N%quadrant3)
@@ -130,6 +139,6 @@ program tree
 
   call insert_particle(p1, root)
   call insert_particle(p2, root)
-  call getpoints(root)
   call insert_particle(p3, root)
+  call getpoints(root)
 end program
